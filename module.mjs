df886ab3915ea08fa4a15fde079a59b1bca6abed
cb <digplan@outlook.com>
2022-10-14 18:53:38 -0500

    more

--- module.mjs
+++ module.mjs
@@ -1,31 +1,50 @@
-//import os from 'node:os'
-//console.log(os.networkInterfaces())
-
-/*
-(can be written to, sends updates to all nodes and must get full acceptance to commit)
-write-nopersist    write-nopersist   write-nopersist
-
-(only receives incoming updates, can be read from)
-read-nopersist    read-nopersist    read-nopersist
-*/
-
-const {ip, port, network, peers, subset, persist, read, write} = process.env
-if (!ip) throw Error('must specify an IP')
-if (!port) throw Error('must specify an port')
-if (!network) throw Error('must specify an network')
-if (!peers) throw Error('must specify peers')
-if (!persist) throw Error('must specify persist')
-if (!read) throw Error('must specify read')
-if (!write) throw Error('must specify write')
-
-const db = {}
-peers.split(',').forEach(peerip => {
-
-})
-
 import serve from 'instaserve'
-serve({
-    api(r, s, data) {
-        
-    }
-}, port, ip)
+import get from 'instax'
+
+const { type, parent, subset } = process.env
+let db = {}
+
+const servertypes = {}
+
+if (type === 'readonly') {
+    db = get(`http://${parent}/state/${subset}`)
+
+    serve({
+        get(r, s, data) {
+            const key = r.url.split('/').pop()
+            return db[key]
+        },
+        nupdate(r, s, data) {
+            const key = r.url.split('/').pop()
+            db[key] = data
+        }
+    })
+}
+
+if (type === 'writeonly') {
+    serve({
+        post(r, s, data) {
+            const key = r.url.split('/').pop()
+            return db[key]
+        },
+    update(r, s, data) {
+            const key = r.url.split('/').pop()
+            db[key] = data
+        },
+    delete(r, s, data) {
+            const key = r.url.split('/').pop()
+            return db[key]
+        },
+    nupdate(r, s, data) {
+            const key = r.url.split('/').pop()
+            db[key] = data
+        })
+}
+
+if (type === 'leader') {
+    serve({
+        candidate(r, s, data) {
+            const key = r.url.split('/').pop()
+            db[key] = data
+        })
+}
